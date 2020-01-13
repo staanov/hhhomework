28e8b71ef53fb02413323eb19d115ab88c1de95a
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2020-01-13 22:39:48 +0300

    Decorators task was done

--- core/decorators.py
+++ core/decorators.py
@@ -1,5 +1,10 @@
-def cache_decorator():
+def cache_decorator(func):
     # Реализовать декоратор который кэширует результаты вызова функции (есть в лекции)
     # Применить для функции calculator (в calculator.py уже есть import функции cache_decorator)
     # Настоятельно прошу написать декоратор руками, а не копировать, т.к. важно запомнить как это работает
-    pass
+    cache = {}
+    def inner_func(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache
+    return inner_func()
