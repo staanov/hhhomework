db82ea2a4e8b1d8b9e58979ddf08cc6aa1b7349b
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2020-01-13 22:30:56 +0300

    Generators task was done

--- core/generators.py
+++ core/generators.py
@@ -1,5 +1,10 @@
+import random
+
 # написать генераторную функцию, которая принимает число N возвращает N рандомных чисел от 1 до 100
 def gen(N):
-    pass
+    for i in range(N):
+        yield random.randint(1, 100)
 
 # написать генераторное выражение, которое делает то же самое
+N = 3
+(random.randint(1, 100) for i in range(N))
