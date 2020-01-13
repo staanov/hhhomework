daa0fee21ff7c283793a8bd0c6d564a809f4efee
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2020-01-13 23:10:57 +0300

    Calculator task was done

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,43 @@
 from decorators import cache_decorator
 
+@cache_decorator
 def calculator(a, b, operation):
     # Здесь нужно реализовать функцию,
     # которая реализует основные арифметические операции между числами: +, -, /, *, **.
     # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+    operations = ['+', '-', '/', '*', '**']
+    if operation not in operations:
+        print("Такой операции нет. Введите данные ещё раз. "
+              "Удостоверьтесь, что выбран один из этих операторов: +, -, /, *, **")
+        return None
+    if operation == "+":
+        return a + b
+    elif operation == "-":
+        return a - b
+    elif operation == "*":
+        return a * b
+    elif operation == "/":
+        return a / b
+    elif operation == "**":
+        return a**b
 
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    while True:
+        try:
+            a = int(input('Введите первое число: '))
+            break
+        except ValueError:
+            print("Вы ввели не число. Попробуйте ещё раз")
+            continue
+    while True:
+        try:
+            b = int(input('Введите второе число: '))
+            break
+        except ValueError:
+            print("Вы ввели не число. Попробуйте ещё раз")
+            continue
+    operation = input('Введите операцию: ')
 
     print('Результат: ', calculator(a, b, operation))
+
